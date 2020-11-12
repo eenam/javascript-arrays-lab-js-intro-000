49b4eafb917c0e8859fa5352ed9d8c12cff3d648
Emma Nam <en2458@gmail.com>
2020-11-12 17:29:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,39 +1,33 @@
 var kittens = ["Milo", "Otis", "Garfield"];
 
-function destructivelyAppendKitten(name){
+function destructivelyAppendKitten(name) {
   kittens.push(name);
-  return name
 }
 
-function destructivelyPrependKitten(name){
+function destructivelyPrependKitten(name) {
   kittens.unshift(name);
-  return name
 }
 
-function destructivelyRemoveLastKitten(){
+function destructivelyRemoveLastKitten() {
   kittens.pop();
-  return kittens
 }
 
-function destructivelyRemoveFirstKitten(){
-  kittens = kittens.slice(1);
-  return kittens
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
 }
 
-function appendKitten(name){
-  var newKittensArray = kittens.concat(name);
-  return newKittensArray
+function appendKitten(name) {
+  return [...kittens, name];
 }
 
-function prependKitten(name){
-  var newArray = [name, ...kittens];
-  return newArray
+function prependKitten(name) {
+  return [name, ...kittens];
 }
 
-function removeLastKitten(){
-  return kittens.slice(0, kittens.length -1);
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1);
 }
 
-function removeFirstKitten(){
-  return kittens.slice (-2);
+function removeFirstKitten() {
+  return kittens.slice(1);
 }
